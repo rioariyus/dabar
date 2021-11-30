ef28c1bbf89e1623edd1e167f7b3710671ca3f0e
rioariyus <akunqonqu@gmail.com>
2021-11-30 09:06:54 +0700

    Dabar php

---
+++ api/read.php
@@ -0,0 +1,28 @@
+<?php
+
+include "../config/koneksi.php";
+
+$data = [];
+
+$query = mysqli_query($kon, "SELECT * FROM 'barang' ORDER BY Id DESC");
+if($query){
+    $status = true;
+    $pesan = "reaquwst success";
+    while($row = mysqli_fetch_assoc($query)){
+        array_push($data, $row);
+    }
+}else{
+    $status = false;
+    $pesan = "reaquest failed";
+}
+
+$json = [
+    "status" => $status,
+    "pesan" => $pesan,
+    "data" => $data
+];
+
+header("content-Type: application/json");
+echo json_encode($json);
+
+?>
